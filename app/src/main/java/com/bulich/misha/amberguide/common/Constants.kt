a93a3b1cbc1e478252cc7e8351439aea737f6560
MikhailIgnatovich <bulich198922@gmail.com>
2022-08-09 00:34:28 +0300

    Добавил Constants.kt в data и константы для коллекций

---
+++ app/src/main/java/com/bulich/misha/amberguide/common/Constants.kt
@@ -0,0 +1,8 @@
+package com.bulich.misha.amberguide.common
+
+object Constants {
+
+    const val HOTELS_COLLECTION = "hotel"
+    const val RESTAURANTS_COLLECTION = "restaurants"
+    const val SIGHTS_COLLECTION = "attractions"
+}
--- app/src/main/java/com/bulich/misha/amberguide/data/remote/firestore_db/CategoryFirestoreDb.kt
+++ app/src/main/java/com/bulich/misha/amberguide/data/remote/firestore_db/CategoryFirestoreDb.kt
@@ -1,5 +1,6 @@
 package com.bulich.misha.amberguide.data.remote.firestore_db
 
+import com.bulich.misha.amberguide.common.Constants
 import com.bulich.misha.amberguide.data.remote.firestore_db.dto.HotelDto
 import com.bulich.misha.amberguide.data.remote.firestore_db.dto.RestaurantDto
 import com.bulich.misha.amberguide.data.remote.firestore_db.dto.SightDto
@@ -14,24 +15,25 @@ class CategoryFirestoreDb {
 
     suspend fun getHotels(): List<HotelDto> {
         return try {
-            db.collection("hotel").get().await().toObjects(HotelDto::class.java)
-        }catch(e: Exception) {
+            db.collection(Constants.HOTELS_COLLECTION).get().await().toObjects(HotelDto::class.java)
+        } catch (e: Exception) {
             emptyList<HotelDto>()
         }
     }
 
     suspend fun getRestaurants(): List<RestaurantDto> {
         return try {
-            db.collection("restaurants").get().await().toObjects(RestaurantDto::class.java)
-        }catch(e: Exception) {
+            db.collection(Constants.RESTAURANTS_COLLECTION).get().await()
+                .toObjects(RestaurantDto::class.java)
+        } catch (e: Exception) {
             emptyList<RestaurantDto>()
         }
     }
 
     suspend fun getSights(): List<SightDto> {
         return try {
-            db.collection("attractions").get().await().toObjects(SightDto::class.java)
-        }catch(e: Exception) {
+            db.collection(Constants.SIGHTS_COLLECTION).get().await().toObjects(SightDto::class.java)
+        } catch (e: Exception) {
             emptyList<SightDto>()
         }
     }
